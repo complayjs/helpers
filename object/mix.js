b299f8e544d184068273e3f5a91a97731e938871
Podolski, Henri <henrip@gmail.com>
2016-06-13 23:31:18 +0200

    Add method advice methods keep and override to mixins

--- object/mix.js
+++ object/mix.js
@@ -1,20 +1,33 @@
 export default function mix(Parent) {
-	
+
 	class Mixed extends Parent {}
 
 	return {
+		keepings: ['constructor'],
+		overrides: [],
+		keep(...args) {
+			this.keepings.concat(args);
+			return this;
+		},
+		override(...args) {
+			this.overrides.concat(args);
+			return this;
+		},
 		Mixed,
 		with(...mixins) {
 			
 			for (let mixin of mixins) {
 				for (let prop in mixin) {
-					Mixed.prototype[prop] = mixin[prop];
+					// not included in keepings or included in overrides
+					if (!~this.keepings.indexOf(prop) || ~this.overrides.indexOf(prop)) {
+						Mixed.prototype[prop] = mixin[prop];	
+					}
 				}
 
 				let protoProps = Object.getOwnPropertyNames(mixin.prototype);
 
 				for (let prop in protoProps) {
-					if (protoProps[prop] !== 'constructor' && mixin.prototype[protoProps[prop]]) {
+					if ((!~this.keepings.indexOf(prop) || ~this.overrides.indexOf(prop)) && mixin.prototype[protoProps[prop]]) {
 						Mixed.prototype[protoProps[prop]] = mixin.prototype[protoProps[prop]];
 					}
 				}
